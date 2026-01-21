798cc888b8bdd80a38f684744abe85dcf6e9dca7
Mohammad Khusnul Khuluq <59632952+khulqu15@users.noreply.github.com>
2026-01-21 14:19:39 +0700

    change vas config file

--- Mqtt/src/mqttc.py
+++ Mqtt/src/mqttc.py
@@ -85,19 +85,9 @@ class MQTT:
     def main_run(self):
         asyncio.run(self.__async_task())
 
-    def __on_disconnected(self, client, userdata, flags, rc, properties):
-        if rc != 0:
-            try:
-                Logger.warning("Unexpected disconnection. Reconnecting...")
-                client.reconnect()
-                self.wait_for_connection()
-                self.subscribe("unregister")
-                self.subscribe("register")
-                self.subscribe("who")
-            except Exception as e:
-                Logger.error(f"Reconnection failed: {e}")
-                self.is_connect = False
-
+    def __on_disconnected(self, client, userdata, rc):
+        Logger.warning(f"MQTT Disconnected rc={rc}")
+        self.is_connect = False
 
     def __on_socket_close(self, client, userdata, sock):
         Logger.critical("Socket closed. Please check the network connection.")
@@ -172,7 +162,6 @@ class MQTT:
             return
 
 
-        # Get device local and global IP
         elif msg.topic == 'who':
             _dev_info = f'device_id:{self.device_id}, host:{get_host()},network:['
             _all_ip = get_ip_addresses()
