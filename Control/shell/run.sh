542027eb00f1b3965310add24831861c3018efd2
Mohammad Khusnul Khuluq <59632952+khulqu15@users.noreply.github.com>
2026-01-22 13:03:32 +0700

    add docker run in run.sh control

--- Control/shell/run.sh
+++ Control/shell/run.sh
@@ -2,6 +2,7 @@
 
 echo "Start ACW TTS-system"
 
+sudo docker run -d --name mongo -p 27017:27017 arm64v8/mongo:4.4.18
 sudo pigpiod
 cd ~/voice_machine/Control
 source ~/voice_machine/venv/bin/activate
